752965ea862068b20992c40d403479c62c59d9d9
frankudoags <frankudoags.com>
2026-03-12 23:24:17 +0100

    wip

--- package/src/specs/Preference.nitro.ts
+++ package/src/specs/Preference.nitro.ts
@@ -1,1 +1,15 @@
-import type { HybridObject } from "react-native-nitro-modules";
+import { type HybridObject } from 'react-native-nitro-modules'
+
+export interface Preference extends HybridObject<{
+  ios: 'swift'
+  android: 'kotlin'
+}> {
+  getString(key: string): Promise<string | null>
+  setString(key: string, value: string): Promise<void>
+  getNumber(key: string): Promise<number | null>
+  setNumber(key: string, value: number): Promise<void>
+  getBool(key: string): Promise<boolean | null>
+  setBool(key: string, value: boolean): Promise<void>
+  remove(key: string): Promise<void>
+  clear(): Promise<void>
+}
